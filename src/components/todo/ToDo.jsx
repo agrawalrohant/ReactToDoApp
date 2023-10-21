b169b2c75086b73a914af9de370b81cb6c36a456
Rohant <agrawalrohant@gmail.com>
2023-10-21 16:26:32 +0530

    Adding comments section to demo component lifecycle with the help of useEffect hook

--- src/components/todo/ToDo.jsx
+++ src/components/todo/ToDo.jsx
@@ -1,15 +1,30 @@
 import ListItems from "../listitems/ListItems";
 import InputBox from "../inputbox/InputBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./ToDo.css";
+import axios from "axios";
 
 const ToDo = () => {
   const [tasks, setTasks] = useState([]);
+
+  const [comments, setComments] = useState([]);
 
   const addNewTask = (newTask) => {
     let newTaslList = [...tasks];
     newTaslList.push(newTask);
     setTasks(newTaslList);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let data = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts/1/comments"
+    );
+    console.log(data);
+    setComments(data.data);
   };
 
   const removeTask = (idx) => {
@@ -26,6 +41,22 @@ const ToDo = () => {
       <h1>React ToDo App</h1>
       <InputBox addNewTask={addNewTask} />
       <ListItems parentTaskList={tasks} removeTask={removeTask} />
+      <>
+        <div>Comments : </div>
+        <h5>
+          {comments.length ? (
+            comments.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  {comment.name} - {comment.body}
+                </li>
+              );
+            })
+          ) : (
+            <h5> No data avaiable</h5>
+          )}
+        </h5>
+      </>
     </>
   );
 };
